305e4881eeed6bfc9e1c4eeddbbcb3e4fc75f44c
Henrik Lauritzen <henrik.lauritzen@aurata.com>
2026-02-16 13:46:27 +0100

    'micropython.pyi': Improve type-annotation for 'const.'

--- micropython.pyi
+++ micropython.pyi
@@ -1,15 +1,20 @@
 # SPDX-License-Identifier: MIT OR CC0-1.0
 
-from typing import Any, Callable, Optional
+from typing import Any, Callable, Optional, TypeVar
+
+_N = TypeVar("_N", int, float)
 
 alloc_emergency_exception_buf: Callable[[int], None]
-const: Callable[[int], int]
 heap_lock: Callable[[], None]
 heap_locked: Callable[[], bool]
 heap_unlock: Callable[[], None]
 kbd_intr: Callable[[int], None]
 schedule: Callable[[Callable[[Any], Any], Any], Any]
 stack_use: Callable[[], None]
+
+
+def const(number: _N) -> _N:
+    return number
 
 
 def mem_info(verbose: bool = False) -> None:
